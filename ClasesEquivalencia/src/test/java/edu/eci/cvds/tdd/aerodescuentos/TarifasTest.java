0bce2afb33fd8c463fcaf50d9ee48ffcfa082b94
johanrueda <johan.rueda@mail.escuelaing.edu.co>
2020-08-26 17:30:05 -0500

    Solucionando errores

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -7,54 +7,54 @@ public class TarifasTest {
 	@Test
     public void edadIncorrecta() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, -5);
-        Assert.assertEquals(0, tarifaF);
+        Assert.assertEquals(0, tarifaF,0);
     }
 	
 	@Test
     public void menorDeEdad() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, 16);
-        Assert.assertEquals(19000, tarifaF);
+        Assert.assertEquals(19000, tarifaF,0);
     }
 	
 	@Test
     public void sinDescEdad() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, 50);
-        Assert.assertEquals(20000, tarifaF);
+        Assert.assertEquals(20000, tarifaF,0);
     }
 	
 	@Test
     public void terceraEdad() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, 66);
-        Assert.assertEquals(18400, tarifaF);
+        Assert.assertEquals(18400, tarifaF,0);
     }
 	
 	@Test
     public void diasIncorrecta() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, -5, 50);
-        Assert.assertEquals(0, tarifaF);
+        Assert.assertEquals(0, tarifaF,0);
     }
 	
 	@Test
     public void descDias() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 15, 50);
-        Assert.assertEquals(17000, tarifaF);
+        Assert.assertEquals(17000, tarifaF,0);
     }
 	
 	@Test
     public void sinDescDias() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, 50);
-        Assert.assertEquals(20000, tarifaF);
+        Assert.assertEquals(20000, tarifaF,0);
     }
 	
 	@Test
     public void tarifaIncorrecta() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(-20000, 21, 50);
-        Assert.assertEquals(0, tarifaF);
+        Assert.assertEquals(0, tarifaF,0);
     }
 	
 	@Test
     public void sinDesc() {
         double tarifaF = CalculadorDescuentos.calculoTarifa(20000, 21, 50);
-        Assert.assertEquals(20000, tarifaF);
+        Assert.assertEquals(20000, tarifaF,0);
     }
 }
